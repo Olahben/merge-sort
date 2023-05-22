9f08bd0f152cb3728381ec8016407a13d7386916
Ola <ohagenbendixen@gmail.com>
2023-05-22 16:56:41 +0200

    Finish merging algorithm

--- app.js
+++ app.js
@@ -1,16 +1,28 @@
 const arr = [5, 3, 8, 6, 4, 1, 7, 2];
 
-function merge(left, right) {
+function merge(left, right, m, n) {
   const sorted = [];
   let i = 0,
     j = 0,
     k = 0;
 
-  if (left[i] < right[j]) {
-    sorted[k] = left[i];
-  } else {
-    
+  while (i <= m && j <= n) {
+    if (left[i] < right[j]) {
+      sorted[k++] = left[i++];
+    } else {
+      sorted[k++] = right[j++];
+    }
   }
+
+  for (; i <= m; i++) {
+    sorted[k++] = left[i];
+  }
+
+  for (; j <= n; j++) {
+    sorted[k++] = right[j];
+  }
+
+  return sorted;
 }
 
 function mergeSort(list) {
@@ -20,8 +32,10 @@ function mergeSort(list) {
     const mid = list.length / 2;
     const left = mergeSort(list.slice(0, mid));
     const right = mergeSort(list.slice(mid, list.length));
-    return merge(left, right);
+    if (left === undefined) return list;
+    if (right === undefined) return list;
+    return merge(left, right, left.length - 1, right.length - 1);
   }
 }
 
-mergeSort(arr);
+console.log(mergeSort(arr));
